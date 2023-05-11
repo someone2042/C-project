09ce21208a328d14e1f14712d46594fbd8761cd5
mohamed <mehdaoui200309@gmail.com>
2023-05-11 18:01:42 +0200

    modify

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,63 +1,63 @@
-// typedef struct{
-//     int jour;
-//     int anne;
-//     int mois;
-// } date;
+typedef struct{
+    int jour;
+    int anne;
+    int mois;
+} date;
 
-// typedef struct{
-//     char Nom[30];
-//     char prenom[30];
-//     date naissance;
-//     int note[5];
-//     float moyenne;
-// } stagiaire;
-// typedef struct{
-//     int lenght;
-//     stagiaire table[100];
-// }classe;
-// typedef struct{
-//     int lenght;
-//     classe table[30];
-// }institut;
+typedef struct{
+    char Nom[30];
+    char prenom[30];
+    date naissance;
+    int note[5];
+    float moyenne;
+} stagiaire;
+typedef struct{
+    int lenght;
+    stagiaire table[100];
+}classe;
+typedef struct{
+    int lenght;
+    classe table[30];
+}institut;
 
-// date remplir_date()
-// {
-//     date a;
-//     printf('saisir la date:\n');
-//     scanf("%d%d%d",&a.jour,&a.mois,&a.anne);
-//     return a ;
-// }
+date remplir_date()
+{
+    date a;
+    printf('saisir la date:\n');
+    scanf("%d%d%d",&a.jour,&a.mois,&a.anne);
+    return a ;
+}
 
-// stagiaire remplir_stage()
-// {
-//     float s;
-//     stagiaire stage;
-//     printf("saisir le nom et le prenom\n");
-//     scanf("%s%s",&stage.Nom,&stage.prenom);
-//     stage.naissance=remplir_date();
-//     for(int i=0;i<5;i++)
-//     {
-//         printf("saisir la not %d \n",i);
-//         scanf("%d",&stage.note[i]);
-//         s=s+stage.note[i];
-//     }
-//     stage.moyenne=s/5;
-//     return stage;
-// }
+stagiaire remplir_stage()
+{
+    float s;
+    stagiaire stage;
+    printf("saisir le nom et le prenom\n");
+    scanf("%s%s",&stage.Nom,&stage.prenom);
+    stage.naissance=remplir_date();
+    for(int i=0;i<5;i++)
+    {
+        printf("saisir la not %d \n",i);
+        scanf("%d",&stage.note[i]);
+        s=s+stage.note[i];
+    }
+    stage.moyenne=s/5;
+    return stage;
+}
 
-// void aficher_stage(stagiaire a)
-// {
-//     printf("le nom est %s: \nle prenom est %s: \nla date de naissance est : %d/%d/%d\nle moyenne est : %f\n",a.Nom,a.prenom,a.naissance.jour,a.naissance.mois,a.naissance.anne,a.moyenne);
-// }
+void aficher_stage(stagiaire a)
+{
+    printf("le nom est %s: \nle prenom est %s: \nla date de naissance est : %d/%d/%d\nle moyenne est : %f\n",a.Nom,a.prenom,a.naissance.jour,a.naissance.mois,a.naissance.anne,a.moyenne);
+}
 
-// classe cree_class()
-// {
-//     classe a;
-//     printf("saisir le nombre des stagaire en class\n");
-//     scanf("%d",&a.lenght);
-//     for(int i=0;i<a.lenght;i++)
-//     {
-//         a.table[i]=remplir_stage();
-//     }
-//     return a;
-// }
+classe cree_class()
+{
+    classe a;
+    printf("saisir le nombre des stagaire en class\n");
+    scanf("%d",&a.lenght);
+    for(int i=0;i<a.lenght;i++)
+    {
+        a.table[i]=remplir_stage();
+    }
+    return a;
+}
