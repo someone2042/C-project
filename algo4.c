a649017481fbca584ba9ad0e28e7ee28b3e73cee
mohamed <mehdaoui200309@gmail.com>
2023-05-11 18:00:26 +0200

    modify

--- algo4.c
+++ algo4.c
@@ -4,7 +4,7 @@ int main(void)
     int c=30;
     int f;
     int t[30]={0,0,2,4,5,102,44,66,93,0,15,23,22,56,2,66,6,2,2,24,15,66,48,32,81,54,43,21,45,76};
-    printf("saisir la valeur que vous voulez suprimer");
+    printf("saisir la valeur que vous voulez suprimer :");
     scanf("%d",&f);
     for(int i=0;i<c;i++)
     {
