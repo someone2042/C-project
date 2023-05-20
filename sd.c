473e20e3e0aa69e83a4e2f1a25d4f3a84ff2c047
mohamed <mehdaoui200309@gmail.com>
2023-05-20 09:06:51 +0200

    first commit

---
+++ sd.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct cellule
+{
+    int exp;
+    float coef;
+    struct cellule *suiv;
+} cellule;
+
+typedef cellule *polynome;
+
+polynome ajoute( polynome poly, int exp ,float coef)
+{
+    polynome nv,p,q;
+    if(poly==NULL)
+    {
+        nv=(polynome)malloc(sizeof(struct cellule));
+        nv->coef=coef;
+        nv->exp=exp;
+        nv->suiv=NULL;
+        
+    }
+}
